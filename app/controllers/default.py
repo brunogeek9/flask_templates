e8778fb35c55a72422df1990bd31b8cadf903ee5
brunogeek9 <brunogeek9@gmail.com>
2019-02-17 16:46:50 -0300

    começando os testes de control flow nos templates

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,6 +1,22 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
 @app.route("/")
 def index():
-    return render_template('basic.html')
+    return render_template('index.html')
 
+@app.route("/report")
+def report():
+    username = request.args.get('username')
+    contuc = 0
+    contlc = 0
+    for lt in username:
+        if lt.islower():
+            contlc = contlc+1
+    for lt in username:
+        if lt.isupper():
+            contuc = contuc+1
+    endn = username[-1].isdigit() 
+    print(endn)
+    print(contlc)
+    print(contuc)
+    return render_template('report.html',username=username,contlc=contlc,contuc=contuc,endn=endn)
